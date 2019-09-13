4e603c028d94e2deba22241c2136fe43c672e036
Daria0412 <s2018w29@icloud.com>
2019-09-13 09:35:25 +0900

    add get_list_time()

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,20 +1,53 @@
+#-*- coding:utf-8 -*-
+
 import json
-from urllib import request as ul
+from datetime import datetime
 
 concert = []
 classic = []
 theater = []
 musical = []
-with open('nogada.json') as json_file:
+festival = []
+timeList = []
+# 저 이제 뭐하죠 어제 또 뭐 필요하다고 했더라
+with open('nogada.json', encoding='utf-8') as json_file:
     json_data = json.load(json_file)
-    for i in range(0,314):
+
+    for i in range(0,1):
         theme = json_data["DATA"][i]["theme"]
         title = json_data["DATA"][i]['title']
-        if theme == "콘서트":
-            concert.append(title)
-        elif theme == "클래식":
-            classic.append(title)
-        elif theme == "연극":
-            theater.append(title)
-        elif theme == "뮤지컬/오페라":
-            musical.append(title)
+        # if theme == "":
+        #     concert.append(title)
+        # elif theme == "":
+        #     classic.append(title)
+        # elif theme == "극":
+        #     theater.append(title)
+        # elif theme == "":
+        #     musical.append(title)
+
+def get_list_theme(self, geted_theme): #테마별 행사 조회
+    for j in range(0,len(json_data)):
+        for i in theme:
+            if geted_theme == i:
+                return json_data["DATA"][j]
+    return {"ok":False}
+
+def get_list_time(st_time, en_time): # 파라미터 st_time 시작시간, en_time 종료 시간
+    now = datetime.now() # 지금의 datetime
+    str_time = now.replace(hour = st_time) # 사용자가 원하는 시작 시간
+    end_time = now.replace(hour = en_time) # 사용자가 원하는 종료 시간
+    for i in range(0,len(json_data)):
+        start = json_data["DATA"][i]["start_date"] + " " + json_data["DATA"][i]["started_at"] + ":00.000000" # 데이터의 시작시간 문자열
+        end = json_data["DATA"][i]["end_date"] + " " + json_data["DATA"][i]["finished_at"] + ":00.000000" # 데이터의 종료시간 문자열
+        str_at = datetime.strptime(start, "%Y-%m-%d %H:%M:%S.%f")
+        end_at = datetime.strptime(end, "%Y-%m-%d %H:%M:%S.%f")
+        if (str_time >= str_at) and (end_time <= end_at): # 12, 14 / 14, 23
+            timeList.append(json_data["DATA"][i])
+            return timeList
+        else : return "해당하는 시간대가 없습니다." # 할 말 없어서 그냥 문자열로 넘김
+
+if __name__=='__main__':
+    print(get_list_time(17,21))
+
+    
+    

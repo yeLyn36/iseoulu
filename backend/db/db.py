f7d2db981ca56a4f5633b7833e4bdfc92d7c67e0
Daria0412 <s2018w29@icloud.com>
2019-09-18 09:17:33 +0900

    add db & operation not yet

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+# from flaskext.mysql import MySQL
+# from flask import Flask
 
-def connect(app):
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'root'
-    app.config['MYSQL_DB'] = 'MyDB'
-    mysql = MySQL(app)
+# mysql = MySQL()
+# def connect(app):  # mysql connection
+#     app.config['MYSQL_HOST'] = 'localhost'
+#     app.config['MYSQL_USER'] = 'root'
+#     app.config['MYSQL_PASSWORD'] = 'mirim2'
+#     app.config['MYSQL_DB'] = 'seoul'
+#     mysql.init_app(app)
+#     global cur
+#     conn = mysql.connect()
+#     cur = conn.cursor()
 
 
+# def login(id, pwd):  # 예외처리해야함.
+#     cur.execute('SELECT id,pwd FROM member where id = ' + id)
+#     rv = cur.fetchall()
+#     return str(rv)
+
+
+# if __name__ == '__main__':
+#     app = Flask(__name__)
+#     connect(app)
+    

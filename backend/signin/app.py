49de6084c304a0a50c5206c7a2e6c1fa5ca96279
yeLyn36 <s2018w11@e-mirim.hs.kr>
2019-09-30 05:48:52 +0900

    modify app.py

--- backend/signin/app.py
+++ backend/signin/app.py
@@ -28,59 +28,49 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/create', methods=['GET', 'POST'])  # db 넣고 없어질 메소드
+@app.route('/create')  # db 넣고 없어질 메소드
 def createMember():
-    if request.method == 'POST':
-        content = request.json
-        id = '%s' % escape(content['id'])
-        pwd = content['pwd']
-        name = content['name']
-        email = content['email']
-        age = content['age']
-        gender = content["gender"]
-        data = sign_in.create_member(id, pwd, name, email, age, gender)
+    content = request.json
+    id = '%s' % escape(content['id'])
+    pwd = content['pwd']
+    name = content['name']
+    email = content['email']
+    age = content['age']
+    gender = content["gender"]
+    data = sign_in.create_member(id, pwd, name, email, age, gender)
         if data is None:
             return "중복되었습니다."
         return jsonify(data)
-    else:
-        return "hi"
-        #jsonify({"ok": False})
+
 
 @app.route('/modifyPwd', methods=['GET', 'POST'])  # db 넣고 없어질 메소드
 def modifyPwd():
-    if request.method == 'POST':
-        content = request.json
-        id = '%s' % escape(content['id'])
-        pwd = '%s' % content['pwd']
-        data = sign_in.modify_password(pwd, id)
-        return jsonify(data)
-    else:
-        return jsonify({"ok": False})
+    content = request.json
+    id = '%s' % escape(content['id'])
+    pwd = '%s' % content['pwd']
+    data = sign_in.modify_password(pwd, id)
+    return jsonify(data)
+
 
 
 @app.route('/modifyAge', methods=['GET', 'POST'])  # db 넣고 없어질 메소드
 def modifyAge():
-    if request.method == 'POST':
-        content = request.json
-        id = '%s' % escape(content['id'])
-        age = '%s' % content['age']
-        data = sign_in.modify_age(age, id)
-        return jsonify(data)
-    else:
-        return jsonify({"ok": False})
+    content = request.json
+    id = '%s' % escape(content['id'])
+    age = '%s' % content['age']
+    data = sign_in.modify_age(age, id)
+    return jsonify(data)
+
 
 
 
 @app.route('/delete', methods=['POST', 'GET'])
 def deleteMember():
-    if request.method == 'POST':
-        content = request.json
-        id = '%s' % escape(content['id'])
-        pwd = '%s' % escape(content['pwd'])
-        sign_in.delete(id, pwd)
-        return jsonify({"ok": False})
-    else:
-        return False
+    content = request.json
+    id = '%s' % escape(content['id'])
+    pwd = '%s' % escape(content['pwd'])
+    sign_in.delete(id, pwd)
+    return jsonify({"ok": False})
 
 
 if __name__ == "__main__":

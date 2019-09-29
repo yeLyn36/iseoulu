7b1307682ae050c4ec85c744861f915b301b75b7
Daria0412 <s2018w29@icloud.com>
2019-09-29 22:48:15 +0900

    modify folder

--- backend/review/service/seoul_service.py
+++ backend/review/service/seoul_service.py
@@ -39,29 +39,6 @@ def delete_review(member_id, pwd):
     session.delete(delete_review)
     session.commit()
     return {"ok":True}
-# def create_location(location):
-#     print(location)
-#     newLocation = Seoul(location)
-#     session.add(newLocation)
-#     session.commit()
-#     return newLocation
-
-# def get_all():
-#     locationList = session.query(Seoul).all()
-#     for i in range(len(locationList)):
-#         locationList[i] = locationList[i].serialize
-#     return locationList
-
-# def get_location(lc_id):
-#     location = session.query(Seoul).filter_by(id=lc_id).first()
-#     location = location.serialize
-#     return location
-
-# def delete(lc_id):
-#     location = session.query(Seoul).filter_by(id=lc_id).first()
-#     session.delete(location)
-#     session.commit()
-#     return {"remove":True}
 
 if __name__ == '__main__':
   pass
